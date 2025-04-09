5a7a27119dd0cd3c52a2afb4ec65572993ad658f
hmztplinv <hmz.tpal@gmail.com>
2025-04-09 18:14:05 +0300

    jwt clean dirs

--- NoteApp.Application/Features/Auth/Dtos/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace NoteApp.Application.Features.Auth.Dtos;
-
-public class LoginRequest
-{
-    public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
-}
--- NoteApp.Application/Interfaces/Services/IJwtService.cs
+++
@@ -1,8 +0,0 @@
-using System.Security.Claims;
-
-namespace NoteApp.Application.Interfaces.Services;
-
-public interface IJwtService
-{
-    string GenerateToken(string username, string role);
-}
